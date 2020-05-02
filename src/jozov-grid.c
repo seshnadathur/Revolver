d55dfe6aedfdc7e91ea51d4a9d20cb6630c84758
seshnadathur <39797132+seshnadathur@users.noreply.github.com>
2020-05-02 14:13:48 +0100

    whitespace

--- src/jozov-grid.c
+++ src/jozov-grid.c
@@ -22,7 +22,7 @@ typedef struct Voxel {
   float dens;
   int nadj;
   long *adj;
-  
+
 } VOXEL;
 
 typedef struct Zone {
@@ -49,11 +49,11 @@ typedef struct ZoneT {
 
 int main(int argc,char **argv) {
 
-  FILE *dens, *zon, *vod, *txt; 
+  FILE *dens, *zon, *vod, *txt;
   VOXEL *p;
   ZONE *z;
   ZONET *zt;
-  char *prefix, *densfile, zonefile[FNL], voidfile[FNL], txtfile[FNL]; 
+  char *prefix, *densfile, zonefile[FNL], voidfile[FNL], txtfile[FNL];
   int h, h2, hl, n, nzones, nrealzones, nhl, nhlcount, nhl2, minusone, adjcount, counter;
   long i, Nvox, nbr_index;
   int ix, iy, iz, j, l, ind, Nside;
@@ -83,7 +83,7 @@ int main(int argc,char **argv) {
   if (sscanf(argv[1],"%c",&obo) == 0) {
     printf("Bad choice of (v)oids or (c)lusters.\n");
     exit(0);
-  }  
+  }
   obo = tolower(obo);
   if (obo == 'v'){
     printf("Finding density minima\n");
@@ -103,7 +103,7 @@ int main(int argc,char **argv) {
     printf("Bad grid size.\n");
     exit(0);
   }
-  Nvox = Nside*Nside*Nside;	/* the total number of voxels (grid cells) */  
+  Nvox = Nside*Nside*Nside;	/* the total number of voxels (grid cells) */
   printf("Total number of voxels: %ld\n",Nvox);
 
   if (obo == 'v'){
@@ -126,7 +126,7 @@ int main(int argc,char **argv) {
     exit(0);
   }
   for (i=0;i<Nvox;i++) {
-    fread(&p[i].dens,1,sizeof(float),dens); 
+    fread(&p[i].dens,1,sizeof(float),dens);
     /* if finding minima, invert the densities and then algorithm proceeds the same */
     if (obo=='c') p[i].dens = 1./p[i].dens;
   }
@@ -210,12 +210,12 @@ int main(int argc,char **argv) {
     }
   }
   printf("Post-jump ...\n"); FF;
-  
+
   /* count the number of zones */
   nzones = 0; nrealzones = 0;
-  for (i = 0; i < Nvox; i++) 
+  for (i = 0; i < Nvox; i++)
     if (numinh[i] > 0) {
-      nzones++;	
+      nzones++;
       /* if (p[i].dens < borderdens) nrealzones++; */
     }
   printf("%d zones found...\n",nzones);
@@ -231,7 +231,7 @@ int main(int argc,char **argv) {
     printf("Unable to allocate zt\n");
     exit(0);
   }
-  
+
   /* initialize zone adjacencies */
   for (h=0;h<nzones;h++) {
     zt[h].nadj = 0;
@@ -252,20 +252,20 @@ int main(int argc,char **argv) {
     } else {
       zonenum[i] = -1;
     }
- 
-  /* Finding particles on zone borders */ 
+
+  /* Finding particles on zone borders */
   printf("Finding zone borders\n");
   for (i = 0; i < Nvox; i++)
     for (j = 0; j < p[i].nadj; j++) {
       testpart = p[i].adj[j];
       if (jumped[i] != jumped[testpart])
-	    zt[zonenum[jumped[i]]].nadj++; /* two neighbouring voxels jump to different zones */
-	  /* could add an edge check here */
-	  if (p[i].nadj < 6) {
-	    zt[zonenum[jumped[i]]].edge = 1;
-	  }
+	      zt[zonenum[jumped[i]]].nadj++; /* two neighbouring voxels jump to different zones */
+	    /* could add an edge check here */
+	    if (p[i].nadj < 6) {
+	      zt[zonenum[jumped[i]]].edge = 1;
+	    }
     }
-  
+
   printf("Allocating zone adjacencies and links\n");
   for (h=0;h<nzones;h++) {
     zt[h].adj = (int *)malloc(zt[h].nadj*sizeof(int));
@@ -323,10 +323,10 @@ int main(int argc,char **argv) {
   }
   printf("Found zone adjacencies\n"); FF;
 
-  /* Free voxel adjacencies */ 
+  /* Free voxel adjacencies */
   for (i=0;i<Nvox; i++) free(p[i].adj);
 
-  /* Use z instead of zt */ 
+  /* Use z instead of zt */
   for (h=0;h<nzones;h++) {
     z[h].nadj = zt[h].nadj;
     z[h].adj = (int *)malloc(zt[h].nadj*sizeof(int));
@@ -355,7 +355,7 @@ int main(int argc,char **argv) {
     inyet2[h] = 0;
   }
 
-  nhl = 0; 
+  nhl = 0;
 
   /* Find the maximum and minimum values of dens */
   maxdens = -BF;
@@ -484,7 +484,7 @@ int main(int argc,char **argv) {
       }
       for (hl = 0; hl < nhl2; hl++) /*reset inyet2 for next round */
 	    inyet2[zonelist2[hl]] = 0;
-      
+
       /* See if there's a beater */
       if (beaten == 1) {
         /* h was beaten, so nothing added to its zonelist */
@@ -506,7 +506,7 @@ int main(int argc,char **argv) {
 	    printf(" %d",nhl); FF;
       }
     } while((lowdens < BF) && (beaten == 0));
-   
+
     z[h].densratio = z[h].leak/p[z[h].core].dens;
 
     fprintf(vod,"0 %lf\n",z[h].densratio); /* Mark the end of the line */
@@ -515,7 +515,7 @@ int main(int argc,char **argv) {
       printf(" h%d:%d\n",h,nhl);
       FF;
     }
-    
+
     z[h].nhl = nhl;	/* total number of zones in this void */
   }
   fclose(vod);
